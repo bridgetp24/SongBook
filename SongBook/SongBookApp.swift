9f6f5ac7708b1207d04f29bd717a11dfde4ce5d8
bridgetp24 <bridgetpeterkin@gmail.com>
2022-04-07 13:05:46 -0500

    Initial Commit

---
+++ SongBook/SongBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongBookApp.swift
+//  SongBook
+//
+//  Created by Bridget Peterkin on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SongBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
